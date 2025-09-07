4f6cec8a0bfb3971fe9f893e2c0c40579126a568
Heet Parikh <heet16@gmail.com>
2025-09-07 22:20:05 +0530

    Add favicon link to layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="%PUBLIC_URL%/favicon.jpg" type="image/jpg">
         <style>{`
 html {
   font-family: ${jetbrainsMono.style.fontFamily};
